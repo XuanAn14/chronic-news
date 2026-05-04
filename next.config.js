f2e9d5c4d7034c48060eae82eeaba1db519dcc1d
Xuan An <anxuan1234@gmail.com>
2026-05-04 17:08:01 +0700

    remotePatterns

--- next.config.js
+++ next.config.js
@@ -6,6 +6,54 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: '**.vnecdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.vnexpress.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.bbci.co.uk',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.bbc.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.reuters.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.apnews.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cnn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.nytimes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.wsj.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.forbes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.yimg.com',
+      },
     ],
   },
 }
